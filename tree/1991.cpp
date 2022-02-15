8c1d1f66a8ab2b02f34e9f0d56e04a264808bcc7
minpaeng <68097132+minpaeng@users.noreply.github.com>
2022-02-16 05:40:07 +0900

    수정 - 1991: 트리 순회

--- tree/1991.cpp
+++ tree/1991.cpp
@@ -1,31 +1,61 @@
 #include <iostream>
 #include <cstring>
+#include <vector>
 using namespace std;
 
-int inOrder[100001];
-int postOrder[100001];
-int idx[100001];
-int n;
+char tree[27];
+int A = 64;
 
-void pre(int postStart, int postEnd, int inStart) {
-	if (postStart > postEnd) return;
-	cout << postOrder[postEnd] << ' ';
-	
-	int frontLen = idx[postOrder[postEnd]] - inStart;
-	pre(postStart, postStart + frontLen - 1, inStart);
-	pre(postStart + frontLen, postEnd - 1, idx[postOrder[postEnd]] + 1);
+void pre(char v, int n) {
+	int i = v - A;
+	if (i > n) return;
+	cout << v;
+	if (tree[i * 2] != '.') {
+		pre(tree[i * 2], n);
+	}
+	if (tree[i * 2 + 1] != '.') {
+		pre(tree[i * 2 + 1], n);
+	}
+}
+
+void mid(char v, int n) {
+	int i = v - A;
+	if (i > n) return;
+	if (tree[i * 2] != '.') {
+		mid(tree[i * 2], n);
+	}
+	cout << v;
+	if (tree[i * 2 + 1] != '.') {
+		mid(tree[i * 2 + 1], n);
+	}
+}
+
+void post(char v, int n) {
+	int i = v - A;
+	if (i > n) return;
+	if (tree[i * 2] != '.') {
+		post(tree[i * 2], n);
+	}
+	if (tree[i * 2 + 1] != '.') {
+		post(tree[i * 2 + 1], n);
+	}
+	cout << v;
 }
 
 int main() {
+	int n;
 	cin >> n;
 	for (int i = 1; i <= n; i++) {
-		cin >> inOrder[i];
-		idx[inOrder[i]] = i;
-	}
-	for (int i = 1; i <= n; i++) {
-		cin >> postOrder[i];
+		char a, b, c;
+		cin >> a >> b >> c;
+		tree[2 * (a - A)] = b;
+		tree[2 * (a - A) + 1] = c;
 	}
 
-	pre(1, n, 1);
+	pre('A', n);
+	cout << '\n';
+	mid('A', n);
+	cout << '\n';
+	post('A', n);
 	return 0;
 }
