a3e6ac652704d981d4373011207f5f8086f2cd60
권민정 <22alswjd@naver.com>
2022-04-23 13:09:23 +0900

    1965: dp로 해결

---
+++ personal/1965.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int box[1001];
+int res[1001];
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> box[i];
+	}
+
+	fill_n(res, n, 1);
+	int max_val = 0;
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < i; j++) {
+			if (box[i] > box[j]) res[i] = max(res[i], res[j] + 1);
+		}
+		max_val = max(max_val, res[i]);
+	}
+
+	cout << max_val;
+	return 0;
+}
